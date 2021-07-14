5190fa665133ab4bd9d17ad35d91ee3fed2f2d8b
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-07-14 06:53:19 +0300

    Update 4-line_up.py

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -4,8 +4,10 @@
 
 def add_arrays(arr1, arr2):
     ''' returns sum of 2 arrays '''
+    if len(arr1) =! len(arr2):
+        return None
     add_arr = []
-    for i in len(arr1):
-         for j in len(arr2):
-             add_arr.append(i + j)
+    for i in range(len(arr1)):
+         for j in range(len(arr2)):
+             add_arr.append(arr1[i] + arr2[j])
     return add_arr
