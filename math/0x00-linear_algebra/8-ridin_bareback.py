d100ece865ccceb23f5ce8c91a06d784d1ed577f
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-07-14 16:25:44 +0300

    Update 8-ridin_bareback.py

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -7,8 +7,13 @@ def mat_mul(mat1, mat2):
     if len(mat1[0]) != len(mat2):
         return None
     result = []
-    for i in range(len(mat1)): 
-        for j in range(len(mat2)):
-            for k in range(len(mat2)):
-                result[i][j] += mat1[i][k] * mat2[j][k]
+    for r in range(len(mat1)):
+        row = []
+        for c in range(len(mat2[0])):
+            i = 0
+            for j in range(len(mat1[0])):
+                k = mat1[r][j] * mat2[j][c]
+                i += k
+            row.append(i)
+        result.append(row)
     return result
