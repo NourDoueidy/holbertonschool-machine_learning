3e003f75a48d35ec71f62cc6bba424b5259f130e
NourDoueidy <nour.doueidy@gmail.com>
2021-07-14 07:15:47 +0300

    completed task

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+''' across the planes '''
+
+
+def add_matices2D(mat1, mat2):
+    ''' returns the sum of 2 matrices '''
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2)[0]:
+        return None
+    sum_mat = []
+    for i in range(len(mat1)):
+        for j in range(len(mat1[0]):
+            sum_mat.append([mat1[i][j] + mat2[i][j])
+    return sum_mat
