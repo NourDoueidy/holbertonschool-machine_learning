ec8eb7de8c127148976b5b9f9c6b3558c6836230
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-08-10 09:22:02 +0300

    Update 1-neuron.py

--- supervised_learning/0x01-classification/1-neuron.py
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -1,36 +1,35 @@
 #!/usr/bin/env python3
-''' Neuron '''
+"""Simple neuron class"""
 
 
 import numpy as np
 
 
 class Neuron:
-    ''' Defines a class Neuron '''
+    """Simple neuron class"""
 
     def __init__(self, nx):
-        ''' Constructor '''
+        """nx: number of input features"""
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
         self.__W = np.ndarray((1, nx))
         self.__W[0] = np.random.normal(size=nx)
         self.__b = 0
-        self.__A
+        self.__A = 0
 
     @property
     def W(self):
-        ''' Weights '''
+        """Return weights"""
         return self.__W
 
     @property
     def b(self):
-        ''' bias '''
+        """Return bias"""
         return self.__b
 
     @property
     def A(self):
-        ''' activation values '''
+        """Return activation values"""
         return self.__A
