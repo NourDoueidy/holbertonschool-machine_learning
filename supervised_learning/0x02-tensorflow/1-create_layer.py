d2a7335f3c2e46ecbfdcc4dec0013a26c72d14ee
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-08-13 11:12:23 +0300

    Update 1-create_layer.py

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-''' Create Layer '''
+"""
+Create a tensorflow layer
+"""
 
 
 import tensorflow as tf
 
 
 def create_layer(prev, n, activation):
-    ''' create a tensorflow layer '''
+    """
+    Create a tensorflow layer
+    """
     initializer = (tf.contrib.layers.
                    variance_scaling_initializer(mode="FAN_AVG"))
     return tf.layers.Dense(n, activation, name='layer',
