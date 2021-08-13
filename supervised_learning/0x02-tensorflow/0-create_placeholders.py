7839661c18cde963d1bc328230f2982e19aafbc1
NourDoueidy <nourdoueidy@hotmail.com>
2021-08-13 10:41:25 +0300

    completed task

---
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+''' Tensorflow placeholders '''
+
+
+import tensorflow as tf
+
+
+def create_placeholders(nx, classes):
+    ''' Create Tensorflow placeholders '''
+    return (tf.placeholder(float, shape=[None, nx], name='x'),
+            tf.placeholder(float, shape=[None, classes], name='y))
